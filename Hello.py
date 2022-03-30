e1ba87c559f322a4a67f31c0efbe22f80b152df4
Harebert Zhu <harebert@gmail.com>
2022-03-30 16:34:12 +0800

    20220330 ADD INDEX.HTML

--- Hello.py
+++ Hello.py
@@ -119,7 +119,63 @@ def img2ExcelBlack(img,Excel):
             worksheet.write(i, j, imageArray[i][j])
     workbook.close()
 
+def img2ExcelColorLBP(img,Excel):
+    # 读取图片，转为excel
+    import xlsxwriter  # 导入模块
+    workbook = xlsxwriter.Workbook(Excel)  # 新建excel表
+    worksheet = workbook.add_worksheet('sheet1')  # 新建sheet（sheet的名称为"sheet1"）
+    worksheet.set_column('A:XFD', 2)  # 设置所有列宽为2
+    worksheet.set_zoom(10)
+    cell_format = workbook.add_format()
 
+    from PIL import Image
+    import numpy as np
+
+    image = Image.open(img)
+    # print(np.array(image))
+
+    imageArray = []
+    for i, I in enumerate(np.array(image)):
+        tempRow = []
+        for j, J in enumerate(I):
+            tempRow.append("#{:0>2s}{:0>2s}{:0>2s}".format(hex(J[0])[2:], hex(J[1])[2:], hex(J[2])[2:]))
+        imageArray.append(tempRow)
+
+    # print(imageArray)
+
+    for i, I in enumerate(imageArray):
+        for j, J in enumerate(I):
+            cell_format = workbook.add_format()
+            cell_format.set_fg_color(J)
+            worksheet.write(i, j, '', cell_format)
+
+    workbook.close()
+def img2ExcelBlackLBP(img,Excel):
+    import xlsxwriter  # 导入模块
+    workbook = xlsxwriter.Workbook(Excel)  # 新建excel表
+    worksheet = workbook.add_worksheet('sheet1')  # 新建sheet（sheet的名称为"sheet1"）
+    worksheet.set_column('A:XFD', 2)  # 设置所有列宽为2
+
+    cell_format = workbook.add_format()
+    cell_format.set_bg_color('red')
+
+    from PIL import Image
+    import numpy as np
+
+    #image = Image.open(img)
+    image=img
+    imageArray = []
+
+    for i, I in enumerate(np.array(image)):
+        tempRow = []
+        for j, J in enumerate(I):
+            tempRow.append(765 - sum(J))
+        imageArray.append(tempRow)
+
+    for i, I in enumerate(imageArray):
+        for j, J in enumerate(I):
+            worksheet.write(i, j, imageArray[i][j])
+    workbook.close()
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -144,8 +200,8 @@ def convolutionScaled():
         if filename.split(".")[1].lower()=="png" or filename.split(".")[1].lower()=="jpg":
 
             f.save(upload_path)
-            session["filename"] = newfilename
-            session["extName"]=filename.split(".")[1]
+            session["CONVfilename"] = newfilename
+            session["CONVextName"]=filename.split(".")[1]
             import PIL.Image as Image
 
             infile = os.path.join(basepath,  'uploads', secure_filename(f.filename))
@@ -174,8 +230,98 @@ def convolutionScaled():
             print(newfilename)
 
             return redirect(url_for('convolutionScaled'))
-    return render_template('upload.html')
+    return render_template('uploadConvolution.html')
+
+@app.route('/uploadLBP', methods=['POST', 'GET'])
+def uploadLBP():
+    def saveTextFile(fileName, image):
+        import numpy as np
+        tempTxt = ""
+        imageArray = []
+        imageArray = np.array(image)
+        for i, I in enumerate(imageArray):
+            for j, J in enumerate(I):
+                tempTxt = tempTxt + str(int(sum(imageArray[i][j])/7.65)) + "\n"
+        txtFileName = os.path.join(basepath, 'static/') + fileName + ".txt"
+        f = open(txtFileName, 'w')
+        f.write(tempTxt)
+        f.close()
+
+    print("this is lbp")
+    import uuid
+    import xlsxwriter  # 导入模块
+    newfilename = str(uuid.uuid1())
+    if request.method == 'POST':
+        print("this is upload")
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        filename = secure_filename(f.filename)
+        upload_path = os.path.join(basepath,  'uploads', secure_filename(f.filename))
+        print(upload_path)
+        if filename.split(".")[1].lower()=="png" or filename.split(".")[1].lower()=="jpg":
+
+            f.save(upload_path)
+            session["LBPfilename"] = newfilename
+            session["LBPextName"]=filename.split(".")[1]
+            import PIL.Image as Image
+            import PIL.ImageEnhance as ImageEnhance
+            infile = os.path.join(basepath,  'uploads', secure_filename(f.filename))
+            outfile = os.path.join(basepath,  'static/') +newfilename+"."+ filename.split(".")[1]
+            outfileB = os.path.join(basepath, 'static/') + newfilename + "B." + filename.split(".")[1]
+            outfileD = os.path.join(basepath, 'static/') + newfilename + "D." + filename.split(".")[1]
+            outfileT4 = os.path.join(basepath, 'static/') + newfilename + "T4." + filename.split(".")[1]
+            #print(outfile)
+            im = Image.open(infile)
 
 
+            (x, y) = im.size  # read image size
+            x_s = 200  # define standard width
+            y_s = 200  # y * x_s / x  # calc height based on standard width
+            out = im.resize((x_s, y_s), Image.ANTIALIAS)  # resize image with high-quality
+
+            en = ImageEnhance.Brightness(out)
+            imDefault=out
+            imBright = en.enhance(1.8)
+            imDark = en.enhance(0.6)
+            imDeep=en.enhance(0.2)
+
+            out.save(outfile)
+            imBright.save(outfileB)
+            imDark.save(outfileD)
+
+
+            #拼多宫格图片
+            IMAGES_FORMAT = ['.png', '.PNG', '.jpg', ".JPG"]  # 图片格式
+            # 获取图片集地址下的所有图片名称
+            #image_names = [name for name in os.listdir(imgDir) for item in IMAGES_FORMAT if os.path.splitext(name)[1] == item]
+            # 定义图像拼接函数
+            to_image = Image.new('RGB', (200, 200))  # 创建一个新图
+            # 循环遍历，把每张图片按顺序粘贴到对应位置上
+            from_image = None
+            from_image = imDeep.resize((100, 100), Image.ANTIALIAS)
+            to_image.paste(from_image, (0, 0))
+            from_image = imDark.resize((100, 100), Image.ANTIALIAS)
+            to_image.paste(from_image, (100, 0))
+            from_image = imDefault.resize((100, 100), Image.ANTIALIAS)
+            to_image.paste(from_image, (0, 100))
+            from_image = imBright.resize((100, 100), Image.ANTIALIAS)
+            to_image.paste(from_image, (100, 100))
+            to_image.save(outfileT4)
+            saveTextFile(newfilename + "T4",to_image)
+            img2ExcelBlackLBP(to_image,os.path.join(basepath,  'static/') +newfilename+"T4.xlsx")
+            saveTextFile(newfilename + "B", imBright)
+            img2ExcelBlackLBP(imBright, os.path.join(basepath,  'static/') +newfilename + "B.xlsx")
+            saveTextFile(newfilename + "D", imDark)
+            img2ExcelBlackLBP(imDark, os.path.join(basepath,  'static/') +newfilename + "D.xlsx")
+            saveTextFile(newfilename + "f", out)
+            img2ExcelBlackLBP(out, os.path.join(basepath,  'static/') +newfilename + "f.xlsx")
+
+
+
+
+
+
+            return redirect(url_for('uploadLBP'))
+    return render_template('uploadLBP.html')
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5050,debug=True)
