9c019419a882a8a7d375b65213feb548be4ae5f8
Harebert Zhu <harebert@gmail.com>
2022-03-31 11:08:13 +0800

    HSV PAGE 20220331

--- Hello.py
+++ Hello.py
@@ -323,5 +323,104 @@ def uploadLBP():
 
             return redirect(url_for('uploadLBP'))
     return render_template('uploadLBP.html')
+
+
+@app.route('/uploadHSV',methods=['POST','GET'])
+def uploadHSV():
+    import uuid
+    import xlsxwriter  # 导入模块
+    newfilename = str(uuid.uuid1())
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        filename = secure_filename(f.filename)
+        upload_path = os.path.join(basepath,  'uploads', secure_filename(f.filename))
+        print(upload_path)
+        if filename.split(".")[1].lower()=="png" or filename.split(".")[1].lower()=="jpg":
+
+            f.save(upload_path)
+            session["HSVfilename"] = newfilename
+            session["HSVextName"]=filename.split(".")[1]
+            import PIL.Image as Image
+
+            infile = os.path.join(basepath,  'uploads', secure_filename(f.filename))
+            outfile = os.path.join(basepath,  'static/') +newfilename+"."+ filename.split(".")[1]
+            print(outfile)
+            im = Image.open(infile)
+            (x, y) = im.size  # read image size
+            x_s = 200  # define standard width
+            y_s = 200  # y * x_s / x  # calc height based on standard width
+            out = im.resize((x_s, y_s), Image.ANTIALIAS)  # resize image with high-quality
+            out.save(outfile)
+
+
+            print(newfilename)
+            workbook = xlsxwriter.Workbook(os.path.join(basepath,  'static/') + newfilename + '.xlsx')  # 新建excel表
+            worksheet = workbook.add_worksheet('sheet1')  # 新建sheet（sheet的名称为"sheet1"）
+            worksheet.set_column('A:XFD', 2)  # 设置所有列宽为2
+
+            cell_format = workbook.add_format()
+            cell_format.set_bg_color('red')
+
+            from PIL import Image
+            import numpy as np
+
+            image = Image.open(outfile)
+            # print(np.array(image))
+            tempTxt=""
+            imageArray = []
+            HArray=[]
+            SArray=[]
+            VArray=[]
+            for i, I in enumerate(np.array(image)):
+                tempRow = []
+                for j, J in enumerate(I):
+                    tempRow.append(765 - sum(J))
+                    H,S,V=rgb2hsv(J[0],J[1],J[2])
+                    HArray.append(int(H/3.6))
+                    SArray.append(int(S*100))
+                    VArray.append(int(V*100))
+                imageArray.append(tempRow)
+
+            #print(imageArray)
+
+            for i, I in enumerate(imageArray):
+                for j, J in enumerate(I):
+                    worksheet.write(i, j, imageArray[i][j])
+                    tempTxt = tempTxt + str(imageArray[i][j]) +"\n"
+            workbook.close()
+            txtFileName=os.path.join(basepath,  'static/') + newfilename+".txt"
+            f = open(txtFileName,'w')
+            f.write(tempTxt)
+            f.close()
+            print(HArray)
+
+
+            tempTxt = ""
+            for i, I in enumerate(HArray):
+                tempTxt = tempTxt + str(I) +"\n"
+            txtFileName=os.path.join(basepath,  'static/') + newfilename+"_H.txt"
+            f = open(txtFileName,'w')
+            f.write(tempTxt)
+            f.close()
+
+
+            tempTxt = ""
+            for i, I in enumerate(SArray):
+                tempTxt = tempTxt + str(I) +"\n"
+            txtFileName=os.path.join(basepath,  'static/') + newfilename+"_S.txt"
+            f = open(txtFileName,'w')
+            f.write(tempTxt)
+            f.close()
+
+            tempTxt = ""
+            for i, I in enumerate(VArray):
+                    tempTxt = tempTxt + str(I) +"\n"
+            txtFileName=os.path.join(basepath,  'static/') + newfilename+"_V.txt"
+            f = open(txtFileName,'w')
+            f.write(tempTxt)
+            f.close()
+            return redirect(url_for('uploadHSV'))
+    return render_template('uploadHSV.html')
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5050,debug=True)
