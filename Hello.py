4ab75ee6728c4a4d7dac42755be0f634536173ce
Harebert Zhu <harebert@gmail.com>
2022-04-10 19:46:25 +0800

    seeRelative.html

--- Hello.py
+++ Hello.py
@@ -467,35 +467,52 @@ def recordRelative():
 
 @app.route('/seeRelative',methods=['POST','GET'])    #正负相关性网页学生作品记录查看
 def seeRelative():
-
-    if request.method == 'GET':
-        #如果是查看GET，则返回所有记录
-        import sqlite3
-        import time
-
-        basepath = os.path.dirname(__file__)
-        datafilePath = os.path.join(basepath, 'Data/Dataset.db')
-        conn = sqlite3.connect(datafilePath)
-
-        c = conn.cursor()
-        content = c.execute("select * from relativeData")
-        record=[]
-        for row in content:
-            tempDic = {}
-            tempDic["id_"]=row[0]
-            tempDic["class_"] = row[1]
-            tempDic["stus_"] = row[2]
-            tempDic["Urls_"] = row[3]
-            tempDic["date_"] = row[4]
-            tempDic["time_"] = row[5]
-            tempDic["score_"] = row[6]
-            #tempDic["id"] = row[0]
-            record.append(tempDic)
-            #print(row)
-        print(record)
-        return render_template('seeRelative.html' ,contents=record)
-    else:
+    import sqlite3
+    #如果有加分减分的动作，则先登分
+    basepath = os.path.dirname(__file__)
+    datafilePath = os.path.join(basepath, 'Data/Dataset.db')
+    conn = sqlite3.connect(datafilePath)
+    c = conn.cursor()
+    if request.method == 'POST':
+        tempDic = {}
+        #print(request.values)
+        for i, I in request.values.items():
+            tempDic[i] = I
+            print(I)
         pass
+
+        if tempDic["action"]=="add":
+            sql = "UPDATE relativeData SET score = score +1  where id=%d" % int(tempDic["id_"])
+        elif tempDic["action"]=="min":
+            sql = "UPDATE relativeData SET score = score -1  where id=%d" % int(tempDic["id_"])
+        print(sql)
+        c.execute(sql)
+
+        conn.commit()
+        # conn.close()
+
+    #如果是查看GET，则返回所有记录
+
+
+
+
+    content = c.execute("select * from relativeData")
+    record=[]
+    for row in content:
+        tempDic = {}
+        tempDic["id_"]=row[0]
+        tempDic["class_"] = row[1]
+        tempDic["stus_"] = row[2]
+        tempDic["Urls_"] = row[3]
+        tempDic["date_"] = row[4]
+        tempDic["time_"] = row[5]
+        tempDic["score_"] = row[6]
+        #tempDic["id"] = row[0]
+        record.append(tempDic)
+        #print(row)
+    print(record)
+    return render_template('seeRelative.html' ,contents=record)
+
         # tempDic["status"] = "Failed"
         # tempDic["url"]="ai.sfls.cn"
        # return render_template('uploadSuccess.html' ,contents=tempDic)
